af79aa64b0d6f9c71c06097e1e62727c45911cc5
Njugunaaa <joshuangich2@gmail.com>
2026-04-29 16:08:20 +0300

    cart modified

--- app/(store)/cart/page.tsx
+++ app/(store)/cart/page.tsx
@@ -1,10 +1,5 @@
-import { ProtectedRoute } from "@/components/ProtectedRoute";
 import Cart from "@/pages/Cart";
 
 export default function CartPage() {
-  return (
-    <ProtectedRoute>
-      <Cart />
-    </ProtectedRoute>
-  );
+  return <Cart />;
 }
